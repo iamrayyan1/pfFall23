2c8857456fa2c13ce3b6edcc7899dae64a6f4e40
iamrayyan1 <142688529+iamrayyan1@users.noreply.github.com>
2023-09-06 14:25:23 +0500

    Update Q3.c

--- Labs/03/Q3.c
+++ Labs/03/Q3.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <math.h>
 
 int main()
@@ -15,4 +15,20 @@ x = a*a + b*b;
 float h =sqrt(x);
 
 printf("hypotenuse is: %f" ,h);
+}*/
+
+
+#include <math.h>
+
+int main()
+{
+int a = 0;
+int b = 0;
+int x = 0;
+printf("Enter base: ");
+scanf(" %d" ,&a);
+printf("Enter Height: ");
+scanf(" %d" ,&b);
+float h = sqrt(pow(a, 2)+pow(b,2));
+printf("hypotenuse is: %f\n" ,h);
 }
