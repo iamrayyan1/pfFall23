ad7d8d17295857f0d58c2326c6d0687b00aa67e4
iamrayyan1 <142688529+iamrayyan1@users.noreply.github.com>
2023-09-07 05:38:35 +0500

    Update 05.c

--- Labs/03/05.c
+++ Labs/03/05.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+ {
+	float a,b,answer;
+	char sign;
+	  printf("Enter First Number: ");
+    scanf("%f", &a);
+    printf("Enter Second Number: ");
+    scanf("%f", &b);
+	  printf("Enter Operator: ");
+    scanf(" %c", &sign);
+  
+    if (sign == '+')
+    {
+    	answer = (a+b);
+    	printf("Answer is: %.2f", answer);
+    }
+	   else if (sign == '-')
+	   {
+    	answer = (a-b);
+    	printf("Answer is: %.2f", answer);
+     }
+   	 else if (sign == '*')
+	   {
+    	answer = (a*b);
+    	printf("Answer is: %.2f", answer);
+     }
+	   else if (sign == '/')
+	   {
+    	answer = (a/b);
+    	printf("Answer is: %.2f", answer);
+	   }
+  return 0;
+}
 
