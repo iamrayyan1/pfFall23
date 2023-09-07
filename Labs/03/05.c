1e8c03ee270d3e60a73f8b68b473b7406684ce8b
iamrayyan1 <142688529+iamrayyan1@users.noreply.github.com>
2023-09-07 22:32:51 +0500

    Update 05.c

--- Labs/03/05.c
+++ Labs/03/05.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <math.h>
 
 int main(void)
@@ -34,4 +34,49 @@ int main(void)
 	   }
   return 0;
 }
+*/
 
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+ {
+float a,b,answer;
+char s;
+printf("Enter First Number: ");
+scanf("%f", &a);
+printf("Enter Second Number: ");
+scanf("%f", &b);
+printf("Enter Operator: ");
+scanf(" %c", &s);
+  
+  switch(s)
+  {
+  	case '+':
+  		answer = a + b;
+  		//printf("answer is: %.2f\n", answer);
+  		break;
+  	case '-':
+	    answer = a - b;
+	    //printf("answer is: %.2f\n", answer);
+	    break;
+	case '*':  
+	    answer = a * b;
+	    //printf("answer is: %.2f\n", answer);
+	    break;
+	case '/':
+		if ( b == 0) {
+	    	printf("Error\n");
+	    }
+		else {
+		
+		answer = a * b;
+		//printf("answer is: %.2f\n", answer);
+	    }
+		break;		
+	default:
+	    printf("Invalid input, try again\n");	    
+    }	    	
+ printf("%.2f %c %.2f = %.2f",a, s, b, answer);
+ return 0;
+}
