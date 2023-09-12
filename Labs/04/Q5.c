db0556de463ae19c0a7112eed43aabe8b117e57a
iamrayyan1 <142688529+iamrayyan1@users.noreply.github.com>
2023-09-13 04:36:04 +0500

    Update Q5.c

--- Labs/04/Q5.c
+++ Labs/04/Q5.c
@@ -1,1 +1,55 @@
+/*
+* Programmer: Rayyan Merchant
+* Date: 12th Sept 2023
+* Description:
+* Related Files:
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int p1_d, p1_m, p1_y, p2_d, p2_m, p2_y;
+	
+	printf("Enter Person 1 DOB in dd/mm/yyyy: ");
+	scanf("%d/%d/%d", &p1_d, &p1_m, &p1_y);
+	printf("Enter Person 2 DOB in dd/mm/yyyy: ");
+	scanf("%d/%d/%d", &p2_d, &p2_m, &p2_y);
+	
+	if (p1_y = p2_y)
+	{
+		if (p1_m = p2_m) 
+		{
+			if (p1_d = p2_d)
+			{
+				printf("Same Age \n");
+			}
+			else if (p1_d > p2_d)
+			{
+				printf("Person 2 is older \n");
+			}
+			else
+			{
+				printf("Person 1 is older \n");
+			}	
+	    } 
+		    else if (p1_m > p2_m)
+			{
+				printf("Person 2 is older \n");
+			}
+			else
+			{
+				printf("Person 1 is older \n");
+		    }
+	}
+	        else if (p1_y > p2_y)
+			{
+				printf("Person 2 is older \n");
+			}
+			else
+			{
+				printf("Person 1 is older \n");
+		    }
+}
 
