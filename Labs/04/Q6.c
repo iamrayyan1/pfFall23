d268ae2e06cf724c3e4503fc35ab906aea53a730
iamrayyan1 <142688529+iamrayyan1@users.noreply.github.com>
2023-09-13 05:33:26 +0500

    Update Q6.c

--- Labs/04/Q6.c
+++ Labs/04/Q6.c
@@ -4,4 +4,73 @@
 * Description:
 * Related Files:
 */
+#include <stdio.h>
+
+int main() {
+    int i = 1, j = 1 ;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    
+    while (i <= n) 
+    {
+        while (j <= n - i) {
+            printf(" ");
+            j++;
+    }
+        printf("*");
+      
+        while (j < 2 * i - 1)
+          {
+            if (j == 1 || j == 2 * i - 2) 
+            {
+                printf(" ");
+            } else 
+            {
+                printf("*");
+            }
+            j++;
+        }
+        if (i != 1)
+        {
+            printf("*");
+        }
+        printf("\n");
+        i++;
+    }
+
+i = n - 1;
+    while (i >= 1)
+      {
+        int j = 1;
+        while (j <= n - i)
+          {
+            printf(" ");
+            j++;
+          }
+        printf("*");
+        j = 1;
+        while (j < 2 * i - 1) 
+        {
+            if (j == 1 || j == 2 * i - 2) 
+            {
+                printf(" ");
+            } else 
+            {
+                printf("*");
+            }
+            j++;
+        }
+        if (i != 1) 
+        {
+            printf("*");
+        }
+        printf("\n");
+
+        i--;
+    }
+
+    return 0;
+}
 
