fd63363d6f07f605e4efd0ac1d183f362430158a
iamrayyan1 <142688529+iamrayyan1@users.noreply.github.com>
2023-09-19 09:06:37 +0500

    Update Q1.c

--- Labs/05/Q1.c
+++ Labs/05/Q1.c
@@ -1,1 +1,18 @@
+#include <stdio.h>;
+int main(void)
+{
+int num = 0, rem = 0;
 
+printf("Enter num: ");
+scanf("%d", &num);
+
+rem = num % 3;
+if (rem == 0)
+{
+	printf("This number is multiple of 3\n");
+}
+else
+{
+	printf("This number is not multiple of 3");
+}
+}
