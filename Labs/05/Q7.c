5235b11929f73e2db0b1477802d93e8b7ec1f237
iamrayyan1 <142688529+iamrayyan1@users.noreply.github.com>
2023-10-02 00:43:04 +0500

    Update Q7.c

--- Labs/05/Q7.c
+++ Labs/05/Q7.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+int main(void)
+{
+	int total = 0, save = 0;
+	int discount = 0;
+	
+	printf("Get upto 50 percent off on Eid Sale\n");
+	
+	printf("Enter Total Amount: ");
+	scanf("%d", &total);
+	
+	if(total >= 2000 && total <= 4000)
+	{
+		save = total * 0.2;	
+	}
+	if(total >= 4001 && total <= 6000)
+	{
+		save = total * 0.3;	
+	}
+	if(total > 6000)
+    {
+
+		save = total * 0.5;	
+	}
+	    
+  (discount = total - save);
+  printf("Total Cost Before Discount: %d\n", total);
+  printf("Amount Saved: %d\n", save);
+  printf("Total Price After Discount: %d\n", discount);
+return 0;
+}
 
