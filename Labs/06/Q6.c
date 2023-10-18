03f07a6c9685393ccdc03d552c0157087be8a047
iamrayyan1 <142688529+iamrayyan1@users.noreply.github.com>
2023-10-19 04:57:22 +0500

    Update Q6.c

--- Labs/06/Q6.c
+++ Labs/06/Q6.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 
 int main () 
 {
@@ -25,6 +25,39 @@ int main ()
         total = total - (5 - n);
         printf ("The number of matchsticks now is: %d\n", total);
         printf("\n"); 
+     }
+
+ printf ("YOU LOSE!! HAHAHAHAA...");
+
+ return 0;
+} */
+
+
+
+#include <stdio.h>
+int main () 
+{
+    int n,a; 
+    int total=21; 
+
+
+     while (total > 1 ) 
+	 {
+        printf("USER'S TURN:\n");
+        printf("There are %d matchsticks. Pick 1, 2, 3, or 4: ", total);
+        scanf("%d", &n);
+         
+        if (n < 1 || n > 4) 
+			 {
+                printf ("Invalid Input\n\n");
+             }
+
+        total = total - n;
+        printf ("The number of matchsticks remaining: %d\n\n", total);
+        printf("COMPUTER'S TURN: \n");
+        a = 5 - n;
+        total = total - a;
+        printf("Matchsticks picked: %d\n\n",a);
      }
 
  printf ("YOU LOSE!! HAHAHAHAA...");
