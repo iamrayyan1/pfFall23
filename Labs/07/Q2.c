b425cc45c8fedf5eac9c4772b91109964136d4b3
iamrayyan1 <142688529+iamrayyan1@users.noreply.github.com>
2023-10-20 04:46:55 +0500

    Update Q2.c

--- Labs/07/Q2.c
+++ Labs/07/Q2.c
@@ -1,1 +1,49 @@
+/*
+* Programmer: Rayyan Merchant
+* Desc: ROTATING AN ARRAY D SPACES
+* Date: 10-10-23
+*
+*/
 
+
+#include <stdio.h>
+int main() 
+{
+    int a, i, j, d, temp;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &a);
+    
+    int arr[a];
+    
+    printf("\nInput:\n");
+    for (i = 0; i < a; i++) 
+	{
+        scanf("%d", &arr[i]);
+    }
+    
+	printf("\nMatrix is:\n");
+	for (i = 0; i < a; i++) 
+	{ 
+        printf("%d\t", arr[i]);
+    }
+    
+    printf("\nEnter number of positions to rotate left: ");
+    scanf("%d", &d);
+
+    for(i=1;i<=d;i++)
+    {
+    	temp  = arr[0];
+    	for(j=0;j<a-1;j++)
+    	{
+    	    arr[j] = arr[j+1];
+		}
+		arr[j] = temp;
+	}
+	
+	printf("\nSorted Matrix is:\n");
+	for (i = 0; i < a; i++) 
+	{
+        printf("%d\t", arr[i]);
+    }
+}
