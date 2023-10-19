e5c7e3d79f882b8abe3f94f7c73f520d2106aa30
iamrayyan1 <142688529+iamrayyan1@users.noreply.github.com>
2023-10-19 05:23:31 +0500

    Update Q7.c

--- Labs/07/Q7.c
+++ Labs/07/Q7.c
@@ -1,3 +1,5 @@
+
+
 /*
 * Programmer: Rayyan Merchant
 * Date: 12th Sept 2023
@@ -5,8 +7,7 @@
 * Related Files:
 */
 
-#include<stdio.h>
-
+/*#include<stdio.h>
 main() 
 { 
 int arr1[2][2],arr2[2][2],ans[2][2];
@@ -49,6 +50,99 @@ for (int i = 0; i < 2; i++)
         }
         printf("\n");
     }
+}*/
+
+
+//MATRIX MULTIPLICATION
+
+#include <stdio.h>
+
+int main() 
+{
+    int a, b, c, d, i, j, k, sum;
+
+    printf("Enter the number of rows of First Matrix: ");
+    scanf("%d", &a);
+    printf("Enter the number of columns First Matrix: ");
+    scanf("%d", &b);
+    
+    printf("Enter the number of rows of Second Matrix: ");
+    scanf("%d", &c);
+    printf("Enter the number of columns Second Matrix: ");
+    scanf("%d", &d);
+    
+    if( b == c )
+    {
+	
+    
+      int arr1[a][b],arr2[c][d],product[a][d];
+    
+      printf("\nInput 1st Matrix:\n");
+      for (i = 0; i < a; i++) 
+	  {
+        for (j = 0; j < b; j++) 
+		{
+            scanf("%d", &arr1[i][j]);
+        }
+      }
+    
+      printf("\nFirst Matrix is:\n");
+      for (i = 0; i < a; i++) 
+	  {
+        for (j = 0; j < b; j++) 
+		{
+            printf("%d\t", arr1[i][j]);
+        }
+        printf("\n");
+      }
+    
+      printf("\nInput 2nd Matrix:\n");
+      for (i = 0; i < c; i++) 
+	  {
+        for (j = 0; j < d; j++) 
+		{
+            scanf("%d", &arr2[i][j]);
+        }
+      }
+    
+      printf("\n2nd Matrix is:\n");
+      for (i = 0; i < c; i++) 
+	  {
+        for (j = 0; j < d; j++) 
+		{
+            printf("%d\t", arr2[i][j]);
+        }
+        printf("\n");
+      }
+      
+      for (i = 0; i < a; i++) 
+	  {
+		
+        for (j = 0; j < d; j++) 
+		{
+			sum = 0;
+			for(k = 0;k<b && k<c; k++)
+			{
+				sum = sum + (arr1[i][k] * arr2[k][j]);
+			}
+			product[i][j] = sum;
+		}
+      }
+    
+      printf("\nMatrix Multiplication is:\n");
+      for (i = 0; i < a; i++) 
+	  {
+        for (j = 0; j < d; j++) 
+		{
+            printf("%d\t", product[i][j]);
+        }
+        printf("\n");
+      }
+    }
+     else
+    {
+    	printf("\nError, Please choose correct rows and column for Matrix Multiplication");
+	}
 }
 
 
