c3127834cb24d9aa9446b2c225a403e85467f58e
iamrayyan1 <142688529+iamrayyan1@users.noreply.github.com>
2023-10-23 16:11:20 +0500

    Create Q8.c

---
+++ Labs/07/Q8.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void) 
+{
+    int a,arr[10000],i,j,temp;
+    
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &a);
+
+    for(i=0;i<a;i++) 
+	{
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    for(i=0;i<a;i++)
+    {
+    	for(j=i+1;j<a;j++)
+    	{
+    	  if(arr[i]>arr[j])
+          {
+		    temp = arr[i];
+		    arr[i] = arr[j];
+		    arr[j] = temp;
+		  }
+	    }
+	}
+	printf("\nElements of array is ascending order: \n");
+	for(i=0;i<a;i++)
+	{
+		printf("%d\t",arr[i]);
+    }
+}
