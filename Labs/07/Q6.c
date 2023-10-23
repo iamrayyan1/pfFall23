b5ca076c63b878e79fdfd4942ae5ae9112621eb9
iamrayyan1 <142688529+iamrayyan1@users.noreply.github.com>
2023-10-23 14:51:34 +0500

    Create Q6.c

---
+++ Labs/07/Q6.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(void) 
+{
+    int a,arr[10000],i,j,c,element;
+    
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &a);
+
+    for (int i = 0; i < a; i++) 
+	{
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    
+
+    for (int i=0; i<a;i++) 
+	{
+        element = arr[i];
+        c=0;
+        
+        if (element == -1) 
+		{
+            continue;
+        }
+        
+        for (int j = i; j < a; j++) 
+		{
+            if (arr[j] == element) 
+			{
+                c++;
+                arr[j] = -1;
+            }
+        }
+        
+        printf("frequency of %d = %d \n", element, c);
+    }
+   return 0; 
+}
