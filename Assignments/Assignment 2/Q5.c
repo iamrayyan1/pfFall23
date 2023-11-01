02500564cf0d4e710769125d5cc41e1b4c1e616e
iamrayyan1 <142688529+iamrayyan1@users.noreply.github.com>
2023-11-01 05:48:33 +0500

    Update Q5.c

--- Assignments/Assignment 2/Q5.c
+++ Assignments/Assignment 2/Q5.c
@@ -4,3 +4,16 @@
 * Description: Ramanujan-Hardy Numbers 2-way
 * Related Files:
 */
+
+#include <stdio.h>
+
+int main ()
+{
+    int i,j,k,n;
+    printf("Enter a value for n: ");
+    scanf ("%d",&n);
+    
+    printf ("The ramanujan numbers till %d are:",n);
+
+
+}
