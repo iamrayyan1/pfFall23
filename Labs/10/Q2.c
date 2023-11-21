b54f354fd22c7ca34d0d7bfcfba2b5e5833691b9
iamrayyan1 <142688529+iamrayyan1@users.noreply.github.com>
2023-11-21 09:03:38 +0500

    Create Q2.c

---
+++ Labs/10/Q2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+void swap(void *a, void *b, size_t size) 
+{
+     char *byte_a = (char*)a;
+     char *byte_b = (char*)b;
+
+    while (size--) 
+    {
+        *byte_a ^= *byte_b;
+        *byte_b ^= *byte_a;
+        *byte_a ^= *byte_b;
+
+        byte_a++;
+        byte_b++;
+    }
+}
+
+int main() 
+{
+    int x, y;
+    
+    printf("Enter Num 1: ");
+    scanf("%d",&x);
+    printf("Enter Num 2: ");
+    scanf("%d",&y);
+    
+    printf("Before swap: x = %d, y = %d\n", x, y);
+
+    swap(&x, &y, sizeof(int));
+
+    printf("After swap: x = %d, y = %d\n", x, y);
+
+    return 0;
+}
